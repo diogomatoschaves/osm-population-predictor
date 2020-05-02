82df4d16571b3a4733fd19112281de31e10aa9ef
diogomatoschaves <diogo_chaves@hotmail.com>
2020-05-02 19:00:53 +0100

    Add download files logic

---
+++ download.py
@@ -0,0 +1,53 @@
+import logging
+import os
+
+import boto3
+
+from utils.config_parser import get_config
+from utils.logger import configure_logger
+
+
+def download_dir(client, resource, dist, local, bucket):
+
+    paginator = client.get_paginator('list_objects_v2')
+
+    page_iterator = paginator.paginate(Bucket=bucket)
+
+    for result in page_iterator:
+
+        if result.get('CommonPrefixes') is not None:
+            for subdir in result.get('CommonPrefixes'):
+                download_dir(client, resource, subdir.get('Prefix'), local, bucket)
+
+        for file in result.get('Contents', []):
+
+            file_name = file.get('Key')
+
+            logging.info(f'\tDownloading {file_name}...')
+
+            dest_pathname = os.path.join(local, file_name)
+
+            if not os.path.exists(os.path.dirname(dest_pathname)):
+                os.makedirs(os.path.dirname(dest_pathname))
+            resource.meta.client.download_file(bucket, file.get('Key'), dest_pathname)
+
+
+def download(base_data_dir, bucket):
+
+    logging.info(f'Downloading files and saving in {base_data_dir}/...')
+
+    client = boto3.client('s3')
+    resource = boto3.resource('s3')
+    download_dir(client, resource, '', local=base_data_dir, bucket=bucket)
+
+
+if __name__ == "__main__":
+
+    boto3.set_stream_logger('', logging.ERROR)
+
+    configure_logger(("boto3", "CRITICAL"))
+
+    config = get_config()
+
+    download(config.base_data_dir, config.bucket)
+
--- utils/logger.py
+++ utils/logger.py
@@ -2,15 +2,20 @@ import logging
 import sys
 
 
-def configure_logger():
+def configure_logger(libraries_level=None):
     logger = logging.getLogger()
+
+    if not isinstance(libraries_level, list):
+        libraries_level = []
 
     for handler in logger.handlers[:]:
         logger.removeHandler(handler)
 
     logger.setLevel(logging.INFO)
-    logging.getLogger("urllib3").setLevel(logging.WARNING)
-    logging.getLogger("shapely").setLevel(logging.ERROR)
+
+    for library, level in libraries_level:
+        logging.getLogger(library).setLevel(getattr(logging, level))
+
     log_handler = logging.StreamHandler(sys.stdout)
     logger.addHandler(log_handler)
 
