010171d5b3eebf66436771c80a2af52ade106626
DMATOSC <diogo.matos_chaves@daimler.com>
2020-05-02 19:09:21 +0100

    Add new test data

--- model/tests_data.py
+++ model/tests_data.py
@@ -3,12 +3,14 @@ tests_areas = {
     "berlin": 891.1,
     "isle-of-wight": 380,
     "hamburg": 755.2,
-    "portugal": 91302
+    "portugal": 91302,
+    "lisbon": 100
 }
 
 tests_actual = {
     "berlin": 3.769E6,
     "isle-of-wight": 1.41538E5,
     "hamburg": 1.822E6,
-    "portugal": 10.28E6
+    "portugal": 10.28E6,
+    "lisbon": 504718
 }
